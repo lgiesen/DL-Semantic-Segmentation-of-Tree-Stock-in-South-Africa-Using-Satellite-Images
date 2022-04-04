1cfdd3c8882b15e4c0defc9f2c595966cbf5ef31
lgiesen <leo.richard.giesen@gmail.com>
2022-04-04 14:53:56 +0200

    feat: work on gpu & data

--- config/config.py
+++ config/config.py
@@ -27,41 +27,43 @@ class Configuration:
             self.model_path = f'{self.base_dir}4_Project/saved_models/'
             self.filepath_model = f'{self.model_path}UNet_20220307-0807_AdaDelta_weightmap_tversky_256.h5'
 
-            self.filepath_satellite_exported = self.path_data + '2_satellite/2629BD_2018_exported.tif'
+            self.filepath_satellite_exported = self.path_data + \
+                '2_satellite/2629BD_2018_exported.tif'
             self.filepath_satellite = self.path_data + '2_satellite/2629BD_2018.tif'
             self.path_satellite = f'{self.path_data}2_satellite/'
             self.filepath_satellite_nw = self.path_satellite + 'satellite-nw.tif'
 
             self.path_labeled_data = f'{self.path_data}1_labeled_data/tif/'
-            self.path_labeled_data_qgis = self.path_labeled_data + 'labeled_areas/labeled_areas_extracted_with_QGIS/'
+            self.path_labeled_data_qgis = self.path_labeled_data + \
+                'labeled_areas/labeled_areas_extracted_with_QGIS/'
             self.filepath_label_nw = self.path_labeled_data_qgis + 'labels-22-02-23-nw.tif'
             self.filepath_label_se = self.path_labeled_data_qgis + 'labels-22-02-23-se.tif'
         elif platform == 'win32':
             print('Something went wrong')
-            
+
         self.path_patches = f'{self.path_data}3_patches/'
         # self.path_labeled_data_areas = f'{self.path_labeled_data}labeled_areas/'
         self.path_patches_labels = f'{self.path_patches}satellite_patches.npy'
         self.path_patches_satellite = f'{self.path_patches}label_patches.npy'
 
         self.filepath_label = f'{self.path_labeled_data}labels-22-02-23-compressed.tif'
-        # self.filepath_label_compressed = self.path_labeled_data + 'labels-22-02-23-compressed.tif'
-        self.filepath_labels_bounding_boxes = self.path_labeled_data + 'label_polygons/labels-area-all/labels_bounding_box.shp'
+        self.filepath_labels_bounding_boxes = self.path_labeled_data + \
+            'label_polygons/labels-area-all/labels_bounding_box.shp'
 
         # 2. data preparation
         self.size = 256
         self.overlap = 32
-        self.patch_size = (self.size,self.size,4) # Height * Width * (Input + Output) channels # (256,256,4)
-        
+        # Height * Width * (Input + Output) channels # (256,256,4)
+        self.patch_size = (self.size, self.size, 4)
+
         # 3. training
         self.test_ratio = 0.2
         self.val_ratio = 0.2
         # Shape of the input data, height*width*channel; Here: RGB
-        self.input_shape = (self.size,self.size,3)
+        self.input_shape = (self.size, self.size, 3)
 
         self.BATCH_SIZE = 32
         self.VAL_BATCH_SIZE = 8
         self.VAL_BATCH_SIZE = 8
         self.NB_EPOCHS = 100
         self.MAX_TRAIN_STEPS = 50
-
--- core/UNet_class.py
+++ core/UNet_class.py
@@ -1,12 +1,15 @@
-from tensorflow.keras import models, layers
-from tensorflow.keras import regularizers
 import torch.nn as nn
 import torch.nn.functional as F
+from config import config
+from tensorflow.keras import layers, models, regularizers
+
+config = config.Configuration()
+
 
 class UNetModel(nn.Module):
 
-    def __init__(self, input_shape, input_label_channel, layer_count=64, regularizers = regularizers.l2(0.0001), gaussian_noise=0.1, weight_file = None):
-        #, num_inputs, num_hidden, num_outputs):
+    def __init__(self, input_shape, input_label_channel, layer_count=64, regularizers=regularizers.l2(0.0001), gaussian_noise=0.1, weight_file=None):
+        # , num_inputs, num_hidden, num_outputs):
         """ Method to declare the UNet model.
 
         Args:
@@ -23,68 +26,158 @@ class UNetModel(nn.Module):
         """
         super().__init__()
         input_img = layers.Input(input_shape[1:], name='Input')
-        pp_in_layer  = input_img
 #        pp_in_layer = layers.GaussianNoise(gaussian_noise)(input_img)
 #        pp_in_layer = layers.BatchNormalization()(pp_in_layer)
 
-
-        c1 = layers.Conv2D(1*layer_count, (3, 3), activation='relu', padding='same')(pp_in_layer)
-        c1 = layers.Conv2D(1*layer_count, (3, 3), activation='relu', padding='same')(c1)
+        c1 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(input_img)
+        c1 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(c1)
         n1 = layers.BatchNormalization()(c1)
         p1 = layers.MaxPooling2D((2, 2))(n1)
 
-        c2 = layers.Conv2D(2*layer_count, (3, 3), activation='relu', padding='same')(p1)
-        c2 = layers.Conv2D(2*layer_count, (3, 3), activation='relu', padding='same')(c2)
+        c2 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(p1)
+        c2 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(c2)
         n2 = layers.BatchNormalization()(c2)
         p2 = layers.MaxPooling2D((2, 2))(n2)
 
-        c3 = layers.Conv2D(4*layer_count, (3, 3), activation='relu', padding='same')(p2)
-        c3 = layers.Conv2D(4*layer_count, (3, 3), activation='relu', padding='same')(c3)
+        c3 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(p2)
+        c3 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(c3)
         n3 = layers.BatchNormalization()(c3)
         p3 = layers.MaxPooling2D((2, 2))(n3)
 
-        c4 = layers.Conv2D(8*layer_count, (3, 3), activation='relu', padding='same')(p3)
-        c4 = layers.Conv2D(8*layer_count, (3, 3), activation='relu', padding='same')(c4)
+        c4 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(p3)
+        c4 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(c4)
         n4 = layers.BatchNormalization()(c4)
         p4 = layers.MaxPooling2D(pool_size=(2, 2))(n4)
 
-        c5 = layers.Conv2D(16*layer_count, (3, 3), activation='relu', padding='same')(p4)
-        c5 = layers.Conv2D(16*layer_count, (3, 3), activation='relu', padding='same')(c5)
+        c5 = layers.Conv2D(16*layer_count, (3, 3),
+                           activation='relu', padding='same')(p4)
+        c5 = layers.Conv2D(16*layer_count, (3, 3),
+                           activation='relu', padding='same')(c5)
 
         u6 = layers.UpSampling2D((2, 2))(c5)
         n6 = layers.BatchNormalization()(u6)
         u6 = layers.concatenate([n6, n4])
-        c6 = layers.Conv2D(8*layer_count, (3, 3), activation='relu', padding='same')(u6)
-        c6 = layers.Conv2D(8*layer_count, (3, 3), activation='relu', padding='same')(c6)
+        c6 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(u6)
+        c6 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(c6)
 
         u7 = layers.UpSampling2D((2, 2))(c6)
         n7 = layers.BatchNormalization()(u7)
         u7 = layers.concatenate([n7, n3])
-        c7 = layers.Conv2D(4*layer_count, (3, 3), activation='relu', padding='same')(u7)
-        c7 = layers.Conv2D(4*layer_count, (3, 3), activation='relu', padding='same')(c7)
+        c7 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(u7)
+        c7 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(c7)
 
         u8 = layers.UpSampling2D((2, 2))(c7)
         n8 = layers.BatchNormalization()(u8)
         u8 = layers.concatenate([n8, n2])
-        c8 = layers.Conv2D(2*layer_count, (3, 3), activation='relu', padding='same')(u8)
-        c8 = layers.Conv2D(2*layer_count, (3, 3), activation='relu', padding='same')(c8)
+        c8 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(u8)
+        c8 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(c8)
 
         u9 = layers.UpSampling2D((2, 2))(c8)
         n9 = layers.BatchNormalization()(u9)
         u9 = layers.concatenate([n9, n1], axis=3)
-        c9 = layers.Conv2D(1*layer_count, (3, 3), activation='relu', padding='same')(u9)
-        c9 = layers.Conv2D(1*layer_count, (3, 3), activation='relu', padding='same')(c9)
+        c9 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(u9)
+        c9 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(c9)
 
-        d = layers.Conv2D(len(input_label_channel), (1, 1), activation='sigmoid', kernel_regularizer= regularizers)(c9)
+        d = layers.Conv2D(len(input_label_channel), (1, 1),
+                          activation='sigmoid', kernel_regularizer=regularizers)(c9)
 
         seg_model = models.Model(inputs=[input_img], outputs=[d])
         if weight_file:
             seg_model.load_weights(weight_file)
         seg_model.summary()
 
-    def forward(self, x):
+    def forward(self, input_img, layer_count=64, input_label_channel=None):
+        if input_label_channel is None:
+            input_label_channel = [config.input_shape[2]]
         # Perform the calculation of the model to determine the prediction
-        x = self.linear1(x)
-        x = self.act_fn(x)
-        x = self.linear2(x)
+        # input_img = layers.Input(input_shape[1:], name='Input')
+        #        pp_in_layer = layers.GaussianNoise(gaussian_noise)(input_img)
+        #        pp_in_layer = layers.BatchNormalization()(pp_in_layer)
+
+        c1 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(input_img)
+        c1 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(c1)
+        n1 = layers.BatchNormalization()(c1)
+        p1 = layers.MaxPooling2D((2, 2))(n1)
+
+        c2 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(p1)
+        c2 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(c2)
+        n2 = layers.BatchNormalization()(c2)
+        p2 = layers.MaxPooling2D((2, 2))(n2)
+
+        c3 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(p2)
+        c3 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(c3)
+        n3 = layers.BatchNormalization()(c3)
+        p3 = layers.MaxPooling2D((2, 2))(n3)
+
+        c4 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(p3)
+        c4 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(c4)
+        n4 = layers.BatchNormalization()(c4)
+        p4 = layers.MaxPooling2D(pool_size=(2, 2))(n4)
+
+        c5 = layers.Conv2D(16*layer_count, (3, 3),
+                           activation='relu', padding='same')(p4)
+        c5 = layers.Conv2D(16*layer_count, (3, 3),
+                           activation='relu', padding='same')(c5)
+
+        u6 = layers.UpSampling2D((2, 2))(c5)
+        n6 = layers.BatchNormalization()(u6)
+        u6 = layers.concatenate([n6, n4])
+        c6 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(u6)
+        c6 = layers.Conv2D(8*layer_count, (3, 3),
+                           activation='relu', padding='same')(c6)
+
+        u7 = layers.UpSampling2D((2, 2))(c6)
+        n7 = layers.BatchNormalization()(u7)
+        u7 = layers.concatenate([n7, n3])
+        c7 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(u7)
+        c7 = layers.Conv2D(4*layer_count, (3, 3),
+                           activation='relu', padding='same')(c7)
+
+        u8 = layers.UpSampling2D((2, 2))(c7)
+        n8 = layers.BatchNormalization()(u8)
+        u8 = layers.concatenate([n8, n2])
+        c8 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(u8)
+        c8 = layers.Conv2D(2*layer_count, (3, 3),
+                           activation='relu', padding='same')(c8)
+
+        u9 = layers.UpSampling2D((2, 2))(c8)
+        n9 = layers.BatchNormalization()(u9)
+        u9 = layers.concatenate([n9, n1], axis=3)
+        c9 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(u9)
+        c9 = layers.Conv2D(1*layer_count, (3, 3),
+                           activation='relu', padding='same')(c9)
+
+        d = layers.Conv2D(len(input_label_channel), (1, 1),
+                          activation='sigmoid', kernel_regularizer=regularizers)(c9)
+
+        self.d = layers.Conv2D(len(input_label_channel), (1, 1),
+                               activation='sigmoid', kernel_regularizer=regularizers)(c9)
         return x
