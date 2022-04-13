7d605b2da8002c71bdc3e23f49aff5abd9fcd129
lgiesen <leo.richard.giesen@gmail.com>
2022-04-13 10:04:17 +0200

    unclutter: :fire: rm unused BA code

--- BA_code/backlog/check_bounds.py
+++
@@ -1,9 +0,0 @@
-# check if bounds are valid
-def check_bounds(opened_raster, checked_c_w, checked_c_s, checked_c_e, checked_c_n, ):
-    c_w, c_e = opened_raster.bounds[0], opened_raster.bounds[2]
-    c_n, c_s = opened_raster.bounds[1], opened_raster.bounds[3]
-    print("Bounds (west, south, east, north):", c_w, c_s, c_e, c_n)
-    if checked_c_w < c_w: print("The coordinates are too far west ({}). Increase it between {} and {}".format(checked_c_w, c_w, c_e))
-    if checked_c_e > c_e: print("The coordinates are too far east ({}). Decrease it between {} and {}".format(checked_c_e, c_w, c_e))
-    if checked_c_n < c_n: print("The coordinates are too far north ({}). Decrease it between {} and {}".format(checked_c_n, c_s, c_n))
-    if checked_c_s > c_s: print("The coordinates are too far south ({}). Increase it between {} and {}".format(checked_c_s, c_s, c_n))
--- BA_code/satellite_coverage_resolution.py
+++ BA_code/satellite_coverage_resolution.py
@@ -1,60 +1,41 @@
 from os import listdir
 from os.path import isfile, join
-
 import rasterio
 
 path_satellite = "/home/jovyan/work/satellite_data/"
 all_tif = [f for f in listdir(path_satellite)
            if isfile(join(path_satellite, f)) and f.endswith('.tif')]
-
 # furthest west, furthest east = min_x, max_x
 # furthest north, furthest south = max_y, min_y
-
 # initialize with first window
 filepath = join(path_satellite, all_tif[0])
 src_img = rasterio.open(filepath)
 (min_x, min_y, max_x, max_y) = src_img.bounds
 west_img = south_img = east_img = north_img = all_tif[0]
 xRes, yRes = src_img.res
-min_res = xRes
-max_res = xRes
-
+min_res, max_res = xRes, xRes
 # update coordinate extend, if cur extend exceeds the window
 for file in all_tif:
     filepath = join(path_satellite, file)
     src_img = rasterio.open(filepath)
     # bounds
     (west, south, east, north) = src_img.bounds
-    if west < min_x:
-        min_x = west
-        west_img = file
-    if east > max_x:
-        max_x = east
-        east_img = file
-    if south < min_y:
-        min_y = south
-        south_img = file
-    if north > max_y:
-        max_y = north
-        north_img = file
+    if west < min_x:    min_x = west;    west_img = file
+    if east > max_x:    max_x = east;    east_img = file
+    if south < min_y:   min_y = south;   south_img = file
+    if north > max_y:   max_y = north;   north_img = file
     # resolution
     xRes, yRes = src_img.res
-    if min_res > xRes:
-        min_res = xRes
-        min_res_img = file
-    elif max_res < xRes:
-        max_res = xRes
-        max_res_img = file
+    if min_res > xRes:  min_res = xRes;  min_res_img = file
+    elif max_res < xRes:max_res = xRes;  max_res_img = file
 
 satellite_coverage = (min_x, min_y, max_x, max_y)
-
 
 def get_res(filename):
     src_img = rasterio.open(path_satellite + filename)
     return src_img.res
 
-
 print("Satellite Coverage:", satellite_coverage)
 print("Edge Files:", west_img, south_img, east_img, north_img)
-print(
-    f"Resolution Range: {min_res_img}: {get_res(min_res_img)}, {max_res_img}: {get_res(max_res_img)}")
+print(f"Resolution Range: {min_res_img}: {get_res(min_res_img)}")
+print(f"{max_res_img}: {get_res(max_res_img)}")
